a86538e5742759de6bed434ca9e850f2605b9cc6
yanina <yraspo@gmail.com>
2013-10-30 15:55:22 -0300

    Added translate of images

--- app/admin/images.rb
+++ app/admin/images.rb
@@ -1,4 +1,13 @@
 ActiveAdmin.register Image do
 	menu :label => "Imagenes"		
+	index do
+  	column "Identificador", :id
+  	column "Nombre" do |image|
+      image.name
+    end
+  	column "Creado", :created_at
+  	column "Actualizado", :updated_at
+  	default_actions
+  end	
   
 end
