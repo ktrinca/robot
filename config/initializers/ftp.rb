395175d64ac1c349047d6d00839437940d27ca52
yanina <yraspo@gmail.com>
2014-03-12 09:35:22 -0300

    Added model ftp

---
+++ config/initializers/ftp.rb
@@ -0,0 +1,10 @@
+CarrierWave.configure do |config|
+  config.ftp_host = "ftp.robot.2trweb.com"
+  config.ftp_port = 21
+  config.ftp_user = "u161744936"
+  config.ftp_passwd = "lkjasd164"
+  config.ftp_folder = "/public_html/uploads"
+  config.ftp_url = "http://robot.2trweb.com/uploads"
+  
+  #config.passive = false # false by default
+end
---
+++ db/migrate/20140310152608_change_data_string_for_cases_subtitle.rb
@@ -0,0 +1,7 @@
+class ChangeDataStringForCasesSubtitle < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
